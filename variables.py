17d0ae2dfca709f8c1f536a13babfde82212ebc3
striversist <striversist@qq.com>
2016-10-29 22:11:18 +0800

    [example]add vairable usage. Add ignore file

---
+++ variables.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+
+# Create a Variable, that will be initialized to the scalar value 0.
+state = tf.Variable(0, name="counter")
+
+# Create an Op to add one to `state`.
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+# Variables must be initialized by running an `init` Op after having
+# launched the graph.  We first have to add the `init` Op to the graph.
+init_op = tf.initialize_all_variables()
+
+# Launch the graph and run the ops.
+with tf.Session() as sess:
+    sess.run(init_op)
+    print(sess.run(state))
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state))
+
