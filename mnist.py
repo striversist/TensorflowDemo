d4c90aa17e1d50a71870f9730ce8d66c2ecb8c5e
striversist <striversist@qq.com>
2016-10-30 18:44:41 +0800

    [mnist]example for mnist data getting and training

--- mnist.py
+++ mnist.py
@@ -1,7 +1,9 @@
 from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
 
 mnist = input_data.read_data_sets("/data/mnist/", one_hot=True)
 
+# Example for getting data
 # Load data
 X_train = mnist.train.images
 Y_train = mnist.train.labels
@@ -10,3 +12,25 @@ Y_test = mnist.test.labels
 
 # Get the next 64 images array and labels
 batch_X, batch_Y = mnist.train.next_batch(64)
+
+# Example for training
+sess = tf.InteractiveSession()
+x = tf.placeholder(tf.float32, [None, 784])
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.initialize_all_variables())
+y = tf.matmul(x, W) + b
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+for i in range(1000):
+    batch = mnist.train.next_batch(100)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(accuracy.eval(feed_dict={x: mnist.test.images, y_:mnist.test.labels}))
+
+sess.close()
